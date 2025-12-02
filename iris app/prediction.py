1e5129df11fc0e6a800baa372d5ea106b9c09f35
YohanesChristianSenjaya <christiansenjaya2007@gmail.com>
2025-12-02 12:28:47 +0700

    Delete iris app directory

--- iris app/prediction.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-def predict(data):
-    # Memuat model yang sudah disimpan
-    # Pastikan nama file sesuai dengan yang ada di folder Anda
-    clf = joblib.load("knn_model.sav") 
-    
-    # Melakukan prediksi berdasarkan data input
-    return clf.predict(data)
